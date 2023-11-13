2034cde096eb8afe609ba547c816abeaf8cb48aa
meindre1 <draebondes@gmail.com>
2023-11-13 23:16:33 +0700

    correct file app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,16 @@
 
 
-fetch('/weather?address=gaza').then((response)=>{
-    response.json().then((data)=>{
-       if(data.error){
-         console.log(error)
-       }else{
-        console.log(data.location)
-        console.log(data.forecast)
-       }
+// fetch('http:localhost:3000/weather?address=gaza').then((response)=>{
+//     response.json().then((data)=>{
+//        if(data.error){
+//          console.log(error)
+//        }else{
+//         console.log(data.location)
+//         console.log(data.forecast)
+//        }
        
-    })
-})
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -29,7 +29,7 @@ weatherForm.addEventListener('submit',(e)=>{
    
 
     
-      fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+      fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
           if(data.error){
            messageOne.textContent = data.error
